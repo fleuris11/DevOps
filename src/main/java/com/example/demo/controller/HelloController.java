84d5544a205284db8eea9f1fd34b38f547378758
Fleuris EGBOOU <fleurismeweegboou5@gmail.com>
2026-03-24 14:19:26 +0100

    hello

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello c'est le cours de CI CD";
+	}
 }
